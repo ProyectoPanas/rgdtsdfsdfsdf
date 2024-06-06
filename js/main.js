addd30407fead2657fe5f0e4e321c62f6b3bc988
ProyectoPanas <19thejockers@gmail.com>
2024-06-06 12:38:25 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ js/main.js
@@ -0,0 +1,52 @@
+let slider = document.querySelector('.slider .list');
+let items = document.querySelectorAll('.slider .list .item');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+let dots = document.querySelectorAll('.slider .dots li');
+
+let lengthItems = items.length - 1;
+let active = 0;
+next.onclick = function(){
+    active = active + 1 <= lengthItems ? active + 1 : 0;
+    reloadSlider();
+}
+prev.onclick = function(){
+    active = active - 1 >= 0 ? active - 1 : lengthItems;
+    reloadSlider();
+}
+let refreshInterval = setInterval(()=> {next.click()}, 12000);
+function reloadSlider(){
+    slider.style.left = -items[active].offsetLeft + 'px';
+    // 
+    let last_active_dot = document.querySelector('.slider .dots li.active');
+    last_active_dot.classList.remove('active');
+    dots[active].classList.add('active');
+
+    clearInterval(refreshInterval);
+    refreshInterval = setInterval(()=> {next.click()}, 12000);
+    
+}
+
+dots.forEach((li, key) => {
+    li.addEventListener('click', ()=>{
+         active = key;
+         reloadSlider();
+    })
+})
+window.onresize = function(event) {
+    reloadSlider();
+};
+
+// Función para ocultar el preloader después de 5 segundos y 2 segundos de desvanecimiento
+    function ocultarPreloader() {
+setTimeout(() => {
+    document.getElementById('preloader').style.opacity = 1;
+}, 3000);
+  
+setTimeout(() => {
+    document.getElementById('preloader').style.display = 'none';
+}, 5000);
+}
+
+// Iniciar la función ocultarPreloader al cargar la página
+window.onload = ocultarPreloader;
